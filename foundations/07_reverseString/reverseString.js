5e25c595cf512930925f4795dc3285d5033b5452
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-01-14 21:23:53 +1300

    Finished-js-07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+        let result = "";
+        for (let i = string.length; i > 0; i--) {
+            result += string.slice(i-1, i);
+        }
+        return result;
 };
 
 // Do not edit below this line
