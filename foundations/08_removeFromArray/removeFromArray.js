992a5d0040cc8d582c872f0b80a13ca9b546d12e
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-01-15 20:42:41 +1300

    Finished-js-08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeArgs) {
+    return array.filter(ele => !removeArgs.includes(ele));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// element => element != removeArgs.forEach()
