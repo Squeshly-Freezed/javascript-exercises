3180d79e8c9b91c6fe2e68614ec6aeb1905b5e49
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-01-16 15:40:13 +1300

    Finished-js-09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    let difference = Math.abs(a - b);
+    if (typeof a == "number" && typeof b == "number" && a >= 0 && b >= 0
+        && a % 1 == 0 && b % 1 == 0) {
+        for (let i = 0; i < difference+1; i++) {
+            if (a >= b) {
+                sum += (b + i);
+            } else {
+                sum += (a + i);
+            }
+        }
+        return sum;
+    } else return "ERROR";
 };
 
 // Do not edit below this line
