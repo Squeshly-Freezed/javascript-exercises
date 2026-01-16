0a49a0fe51bc3c97dbd7af362cceed98c51237af
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-01-16 16:49:57 +1300

    Finished-js-10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    if (year % 100 == 0) {
+        return year % 400 == 0;
+    } else return year % 4 == 0;
+}
 // Do not edit below this line
 module.exports = leapYears;
