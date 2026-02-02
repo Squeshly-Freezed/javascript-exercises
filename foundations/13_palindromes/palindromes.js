eb1b5c1625e529117819484da3351bd1efbed58a
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-02 15:15:39 +1300

    Finished-foundations-js-13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const stringToArray = string.toLowerCase().split("");
+    const filteredArray = stringToArray.filter(character => /^[a-z0-9]+$/i.test(character));
+    return (filteredArray.join() == filteredArray.toReversed().join());
 };
 
 // Do not edit below this line
