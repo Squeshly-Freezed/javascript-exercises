fededfb6993630d55104c21ad92a337c4c77f7fa
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-02 16:19:46 +1300

    Finished-foundations-js-14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number, 10);
+    if (number < 0) return "OOPS";
+    else {
+        if (number == 0) return 0;
+        if (number == 1) return 1;
+        return fibonacci(number-1) + fibonacci(number-2);
+    }
 };
 
 // Do not edit below this line
